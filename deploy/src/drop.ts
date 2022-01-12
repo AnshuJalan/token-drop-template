005e44f39e4e98ff2911520c8480cc0c2f028ec3
AnshuJalan <anshujalan206@gmail.com>
2022-01-12 12:26:27 +0530

    fix: merkle data format

---
+++ deploy/src/drop.ts
@@ -0,0 +1,11 @@
+// Modify data according to your drop
+// Data specification:
+//  Tezos address => Number of tokens to receive (including token decimals)
+const data: { [key: string]: string } = {
+  tz1ZczbHu1iLWRa88n9CUiCKDGex5ticp19S: "100",
+  tz1eUzpKnk5gKLYw4HWs2sWsynfbT7ypGxNM: "200",
+  tz1fxRWk1b53H3RLVxuipjCJJghPmzju7zQA: "300",
+  tz1VyBpzPZSpYHpqKzvVHWGs8vSuoiBHmZSN: "400",
+};
+
+export default data;
--- deploy/src/utils/merkleTree.ts
+++ deploy/src/utils/merkleTree.ts
@@ -5,15 +5,16 @@ import { MerkleTree } from "merkletreejs";
 import { TezosToolkit } from "@taquito/taquito";
 
 // Drop data
-import Drop from "../drop.json";
+import Drop from "../drop";
 
 export const buildMerkle = async (tezos: TezosToolkit) => {
   const packedList: string[] = [];
-  for (const item of Drop) {
+  const dropAddresses: any[] = Object.keys(Drop);
+  for (const address of dropAddresses) {
     const pack = await tezos.rpc.packData({
       data: {
         prim: "Pair",
-        args: [{ string: item.address }, { int: item.value }],
+        args: [{ string: address }, { int: Drop[address] }],
       },
       type: {
         prim: "pair",
@@ -26,8 +27,26 @@ export const buildMerkle = async (tezos: TezosToolkit) => {
   const leaves = packedList.map((x) => CryptoJS.SHA256(CryptoJS.enc.Hex.parse(x)));
   const tree = new MerkleTree(leaves, CryptoJS.SHA256, { sort: true });
 
-  const serializedTree = JSON.stringify(tree);
-  fse.outputJSON(path.join(__dirname, "../merkle_build/mrk.json"), serializedTree, (err) => {
+  // Data specification
+  //  Tezos address => Merkle proof & assocaited leaf data (Michelson packed)
+  const mrklData: {
+    [key: string]: {
+      tokens: string;
+      proof: string[];
+      leafDataPacked: string;
+    };
+  } = {};
+
+  // Loop through addresses and store proofs and leaves in mrklData
+  for (let i = 0; i < dropAddresses.length; i++) {
+    mrklData[dropAddresses[i]] = {
+      tokens: Drop[dropAddresses[i]],
+      proof: tree.getHexProof(leaves[i].toString()),
+      leafDataPacked: packedList[i],
+    };
+  }
+
+  fse.outputFile(path.join(__dirname, "../merkle_build/mrklData.json"), JSON.stringify(mrklData), (err) => {
     if (err) console.log(err);
   });
 
